af4d5a8770e02c6a132f0e21618934f8387368c5
edsonribeiro13 <edson.viniciussr@gmail.com>
2022-11-03 23:17:22 -0300

    feature: adição da localStore

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:eventos_uesb/routes/Routes.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(Routes().createRouteTable());
 }
