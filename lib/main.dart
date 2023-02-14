bed791e89623b3ca3956cca53fd8b30f533868ac
edsonribeiro13 <edson.viniciussr@gmail.com>
2023-02-13 22:56:27 -0300

    chore: adição do firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:eventos_uesb/routes/Routes.dart';
 import 'package:eventos_uesb/utils/repository/Querys.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   WidgetsFlutterBinding.ensureInitialized();
   Querys().ddl();
   runApp(Routes().createRouteTable());
